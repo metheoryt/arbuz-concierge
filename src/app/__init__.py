b6809c4fbafe08aadf3d26d6f99f62a51119902d
Maxim <maxim@exactly.ai>
2025-05-11 00:58:02 +0500

    cleanup

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,17 +1,20 @@
 import logging
 
-from app.db import Base, engine
+from app.db import engine
+from app.db.models import Base
 
 LOG_FORMAT = "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
+# temporarily put it here to enable logging unconditionally
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOG_FORMAT,
+    datefmt=DATE_FORMAT,
+)
+
 
 def setup():
-    logging.basicConfig(
-        level=logging.INFO,
-        format=LOG_FORMAT,
-        datefmt=DATE_FORMAT,
-    )
     Base.metadata.create_all(engine)
 
 
--- src/app/db/models/product.py
+++ src/app/db/models/product.py
@@ -25,37 +25,37 @@ class Product(Base):
         back_populates="products",
         lazy="selectin",
     )
-    name: Mapped[str]  # name 'Пробиотик Arbuz Select x Dr.Galamilk из козьего молока 240 мл'
-    producer_country: Mapped[str | None]  # producerCountry 'Казахстан'
-    brand_name: Mapped[str | None]  # brandName 'Arbuz Select x Dr.Galamilk'
-    description: Mapped[str]  # description ''
+    name: Mapped[str]
+    producer_country: Mapped[str | None]
+    brand_name: Mapped[str | None]
+    description: Mapped[str]
     image_url: Mapped[str | None]
-    measure: Mapped[str]  # measure 'шт'
-    is_weighted: Mapped[bool]  # isWeighted False
-    weight_avg: Mapped[float]  # weightAvg 0
-    weight_min: Mapped[float]  # weightMin 1
-    weight_max: Mapped[float]  # weightMax 52
-    weight: Mapped[str | None]  # weight '1 шт'
-    piece_weight_max: Mapped[float]  # pieceWeightMax 0
-    piece_weight_min: Mapped[float]  # pieceWeightMin 1
-    sell_by_piece: Mapped[bool]  # sellByPiece False
-    quantity_min_step: Mapped[float]  # quantityMinStep 1
-    price_actual: Mapped[float]  # priceActual 2090
-    price_special: Mapped[int | None]  # priceSpecial None
-    price_previous: Mapped[str | None]  # pricePrevious 2345
-    is_available: Mapped[bool]  # isAvailable True
+    measure: Mapped[str]
+    is_weighted: Mapped[bool]
+    weight_avg: Mapped[float]
+    weight_min: Mapped[float]
+    weight_max: Mapped[float]
+    weight: Mapped[str | None]
+    piece_weight_max: Mapped[float]
+    piece_weight_min: Mapped[float]
+    sell_by_piece: Mapped[bool]
+    quantity_min_step: Mapped[float]
+    price_actual: Mapped[float]
+    price_special: Mapped[int | None]
+    price_previous: Mapped[str | None]
+    is_available: Mapped[bool]
     is_local: Mapped[bool]
-    nutrition_fats: Mapped[float | None]  # nutrition {'fats': '4,7', 'kcal': '72', 'carbs': '3,7', 'protein': '5,2'}
+    nutrition_fats: Mapped[float | None]
     nutrition_carbs: Mapped[float | None]
     nutrition_protein: Mapped[float | None]
     nutrition_kcal: Mapped[float | None]
-    ingredients: Mapped[str | None]  # ingredients 'Цельное козье молоко, закваска молочных культур'
-    storage_conditions: Mapped[str | None]  # storageConditions '14 дней, от +2&deg;С до +4&deg;С'
+    ingredients: Mapped[str | None]
+    storage_conditions: Mapped[str | None]
     features: Mapped[list["Feature"]] = relationship(
         secondary="product_features",
         back_populates="products",
         lazy="selectin",
     )
     information: Mapped[str]
-    rating_value: Mapped[float | None]  # rating {'value': '4,6', 'reviews': '16 оценок'}
+    rating_value: Mapped[float | None]
     rating_reviews: Mapped[int | None]
